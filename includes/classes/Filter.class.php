2d5bf193a64faff4d12999e7f62d90a5a6d72faf
secondtruth <christian.neff@gmail.com>
2013-02-24 11:27:32 +0100

    Coding style improvements to the Filter class

--- includes/classes/Filter.class.php
+++ includes/classes/Filter.class.php
@@ -24,42 +24,37 @@
 /**
  * Filter vars and check their type
  *
- * @author  Martin Lantzsch <martin@linux-doku.de>
+ * @author   Martin Lantzsch <martin@linux-doku.de>
  */
 class Filter {
 
     /**
      * Validates the value as a boolean value
-     * @param   mixed   $var     The variable to validate
-     * @param   bool    $strict  If this is TRUE, FALSE is returned only for "0", "false", "off", "no", and "", and NULL
-     *                             is returned for all non-boolean values
-     * @return  bool
-     * @access  public
+     * @param    mixed    $var     The variable to validate
+     * @param    bool     $strict  If this is TRUE, FALSE is returned only for "0", "false", "off", "no", and "", and NULL
+     *                               is returned for all non-boolean values
+     * @return   bool
+     * @access   public
      * @static
      */
     public static function isBool($var, $strict = false) {
-        if ($strict) {
-            $optionsArg = FILTER_NULL_ON_FAILURE;
-        } else {
-            $optionsArg = 0;
-        }
+        $optionsArg = $strict ? FILTER_NULL_ON_FAILURE : 0;
         
         return filter_var($var, FITLER_VALIDATE_BOOLEAN, $optionsArg);
     }
 
     /**
      * Validates the value as integer, optionally from the specified range
-     * @param   mixed  $var       The variable to validate
-     * @param   int    $minRange  The minimum range. Optional.
-     * @param   int    $maxRange  The maximum range. Optional.
-     * @param   int    $flags     One or more of the FILTER_FLAG_* flags,
-     *                              see {@link http://www.php.net/manual/en/filter.filters.validate.php}
-     * @return  bool
-     * @access  public
+     * @param    mixed    $var        The variable to validate
+     * @param    int      $minRange   The minimum range. Optional.
+     * @param    int      $maxRange   The maximum range. Optional.
+     * @param    int      $flags      One or more of the FILTER_FLAG_* flags,
+     *                                  see {@link http://www.php.net/manual/en/filter.filters.validate.php}
+     * @return   bool
+     * @access   public
      * @static
      */
     public static function isInt($var, $minRange = null, $maxRange = null, $flags = 0) {
-        $options = array();
         if (isset($minRange))
             $options['min_range'] = $minRange;
         if (isset($maxRange))
@@ -75,16 +70,15 @@ class Filter {
 
     /**
      * Validates the value as float
-     * @param   mixed  $var      The variable to validate
-     * @param   array  $decimal  The decimal point
-     * @param   int    $flags    One or more of the FILTER_FLAG_* flags,
-     *                             see {@link http://www.php.net/manual/en/filter.filters.validate.php}
-     * @return  bool
-     * @access  public
+     * @param    mixed    $var       The variable to validate
+     * @param    array    $decimal   The decimal point
+     * @param    int      $flags     One or more of the FILTER_FLAG_* flags,
+     *                                 see {@link http://www.php.net/manual/en/filter.filters.validate.php}
+     * @return   bool
+     * @access   public
      * @static
      */
     public static function isFloat($var, $decimal = null, $flags = 0) {
-        $options = array();
         if (isset($decimal))
             $options['decimal'] = $decimal;
         
@@ -98,9 +92,9 @@ class Filter {
 
     /**
      * Validates the value as an email adress
-     * @param   mixed  $var  The variable to validate
-     * @return  bool
-     * @access  public
+     * @param    mixed    $var   The variable to validate
+     * @return   bool
+     * @access   public
      * @static
      */
     public static function isEmail($var) {
@@ -109,11 +103,11 @@ class Filter {
 
     /**
      * Validates the value as URL, optionally with required components
-     * @param   mixed  $var    The variable to validate
-     * @param   int    $flags  One or more of the FILTER_FLAG_* flags,
-     *                           see {@link http://www.php.net/manual/en/filter.filters.validate.php}
-     * @return  bool
-     * @access  public
+     * @param    mixed    $var     The variable to validate
+     * @param    int      $flags   One or more of the FILTER_FLAG_* flags,
+     *                               see {@link http://www.php.net/manual/en/filter.filters.validate.php}
+     * @return   bool
+     * @access   public
      * @static
      */
     public static function isURL($var, $flags = 0) {
@@ -122,11 +116,11 @@ class Filter {
 
     /**
      * Validates the value as IP address, optionally only IPv4 or IPv6 or not from private or reserved ranges
-     * @param   mixed  $var    The variable to validate
-     * @param   int    $flags  One or more of the FILTER_FLAG_* flags,
-     *                           see {@link http://www.php.net/manual/en/filter.filters.validate.php}
-     * @return  bool
-     * @access  public
+     * @param    mixed    $var     The variable to validate
+     * @param    int      $flags   One or more of the FILTER_FLAG_* flags,
+     *                               see {@link http://www.php.net/manual/en/filter.filters.validate.php}
+     * @return   bool
+     * @access   public
      * @static
      */
     public static function isIP($var, $flags = 0) {
@@ -135,10 +129,10 @@ class Filter {
 
     /**
      * Checks if a variable matches a regex pattern
-     * @param   mixed   $var      The variable to check
-     * @param   string  $pattern  Match against this pattern
-     * @return  bool
-     * @access  public
+     * @param    mixed    $var       The variable to check
+     * @param    string   $pattern   Match against this pattern
+     * @return   bool
+     * @access   public
      * @static
      */
     public static function matchesRegex($var, $pattern = '') {
@@ -149,9 +143,9 @@ class Filter {
 
     /**
      * Removes all characters (from a string) except digits, plus and minus sign
-     * @param   mixed  $var  The variable to sanitize
-     * @return  int
-     * @access  public
+     * @param    mixed    $var   The variable to sanitize
+     * @return   int
+     * @access   public
      * @static
      */
     public static function sanitizeInt($var) {
@@ -160,11 +154,11 @@ class Filter {
 
     /**
      * Removes all characters (from a string) except digits, plus and minus sign and optionally ., eE
-     * @param   mixed  $var    The variable to sanitize
-     * @param   int    $flags  One or more of the FILTER_FLAG_* flags,
-     *                           see {@link http://www.php.net/manual/en/filter.filters.sanitize.php}
-     * @return  float
-     * @access  public
+     * @param    mixed    $var     The variable to sanitize
+     * @param    int      $flags   One or more of the FILTER_FLAG_* flags,
+     *                               see {@link http://www.php.net/manual/en/filter.filters.sanitize.php}
+     * @return   float
+     * @access   public
      * @static
      */
     public static function sanitizeFloat($var, $flags = 0) {
@@ -173,11 +167,11 @@ class Filter {
 
     /**
      * Strips tags, optionally strips or encodes special characters (in a string)
-     * @param   mixed   $var    The variable to sanitize
-     * @param   int     $flags  One or more of the FILTER_FLAG_* flags,
-     *                            see {@link http://www.php.net/manual/en/filter.filters.sanitize.php}
-     * @return  string
-     * @access  public
+     * @param    mixed    $var     The variable to sanitize
+     * @param    int      $flags   One or more of the FILTER_FLAG_* flags,
+     *                               see {@link http://www.php.net/manual/en/filter.filters.sanitize.php}
+     * @return   string
+     * @access   public
      * @static
      */
     public static function sanitizeString($var, $flags = 0) {
@@ -186,9 +180,9 @@ class Filter {
 
     /**
      * Remove all characters (from a string) except letters, digits and !#$%&'*+-/=?^_`{|}~@.[]
-     * @param   mixed  $var  The variable to sanitize
-     * @return  string
-     * @access  public
+     * @param    mixed    $var   The variable to sanitize
+     * @return   string
+     * @access   public
      * @static
      */
     public static function sanitizeEmail($var) {
@@ -197,9 +191,9 @@ class Filter {
 
     /**
      * Remove all characters (from a string) except letters, digits and $-_.+!*'(),{}|\\^~[]`<>#%";/?:@&=
-     * @param   mixed  $var  The variable to sanitize
-     * @return  string
-     * @access  public
+     * @param    mixed    $var   The variable to sanitize
+     * @return   string
+     * @access   public
      * @static
      */
     public static function sanitizeURL($var) {
@@ -208,11 +202,11 @@ class Filter {
 
     /**
      * Formats a number with grouped thousands
-     * @param   float   $number          The number to be formatted
-     * @param   int     $decimals        Sets the number of decimal points
-     * @param   bool    $groupThousands  Enable grouping of thousands
-     * @return  string
-     * @access  public
+     * @param    float    $number           The number to be formatted
+     * @param    int      $decimals         Sets the number of decimal points
+     * @param    bool     $groupThousands   Enable grouping of thousands
+     * @return   string
+     * @access   public
      * @static
      */
     public static function formatNumber($number, $decimals = 0, $groupThousands = false) {
@@ -226,10 +220,10 @@ class Filter {
 
     /**
      * Formats a number as a currency string
-     * @param   float   $number  The number to be formatted
-     * @param   string  $format  The money_format() format to use. Defaults to '%i'.
-     * @return  string
-     * @access  public
+     * @param    float    $number   The number to be formatted
+     * @param    string   $format   The money_format() format to use. Defaults to '%i'.
+     * @return   string
+     * @access   public
      * @static
      */
     public static function formatCurrency($number, $format = '%i') {
@@ -238,33 +232,33 @@ class Filter {
 
     /**
      * Formats the given time or date
-     * @param   string  $format  The date() format to use
-     * @param   mixed   $input   The time/date to be formatted. Can be DateTime object, UNIX timestamp, MySQL timestamp
-     *                             or date/time string. Defaults to the current time.
-     * @return  string
-     * @access  public
+     * @param    string   $format   The date() format to use
+     * @param    mixed    $input    The time/date to be formatted. Can be DateTime object, UNIX timestamp, MySQL timestamp
+     *                                or date/time string. Defaults to the current time.
+     * @return   string
+     * @access   public
      * @static
      */
     public static function formatTime($format, $input = null) {
         if (!isset($input)) {
-            // no input, use current time
+            // No input, use current time
             $time = time();
         } elseif ($input instanceof DateTime) {
             $time = $input->getTimestamp();
         } elseif (preg_match('/^\d{14}$/', $input)) {
-            // it is MySQL timestamp format of YYYYMMDDHHMMSS
-            $time = mktime(substr($input, 8, 2),substr($input, 10, 2),substr($input, 12, 2),
-                           substr($input, 4, 2),substr($input, 6, 2),substr($input, 0, 4));
+            // MySQL timestamp format of YYYYMMDDHHMMSS
+            $time = mktime(substr($input, 8, 2), substr($input, 10, 2), substr($input, 12, 2),
+                           substr($input, 4, 2), substr($input, 6, 2),  substr($input, 0, 4));
         } elseif (is_numeric($input)) {
-            // it is a numeric string, we handle it as timestamp
+            // Numeric string, we handle it as timestamp
             $time = (int) $input;
         } else {
-            // strtotime should handle it
+            // strtotime() should handle it
             $strtotime = strtotime($input);
             if ($strtotime != -1 && $strtotime !== false) {
                 $time = $strtotime;
             } else {
-                // strtotime() was not able to parse $input, use current time:
+                // strtotime() was not able to parse, use current time
                 $time = time();
             }
         }
