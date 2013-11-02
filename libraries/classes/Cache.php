f4beecfabc5ca859121597ca0357b1d25a653b63
secondtruth <christian.neff@gmail.com>
2013-11-02 22:16:18 +0100

    Added new caching implementation (class Cache)

---
+++ libraries/classes/Cache.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Webwork
+ * Copyright (C) 2011 IceFlame.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @package     Webwork
+ * @version     0.1-dev
+ * @link        http://www.iceflame.net
+ * @license     ISC License (http://www.opensource.org/licenses/ISC)
+ */
+
+define('WW_CACHE_PATH', WW_ENGINE_PATH.'/cache/'.WW_SITE_NAME);
+
+/**
+ * Class for reading and storing cache files
+ *
+ * @author   Christian Neff <christian.neff@gmail.com>
+ */
+class Cache {
+
+    /**
+     * The name of the cache file
+     * @var      string
+     * @access   private
+     */
+    private $_name;
+
+    /**
+     * The lifetime of the cache file in seconds
+     * @var      int
+     * @access   private
+     */
+    private $_lifetime;
+
+    /**
+     * Constructor
+     * @param    string   $name       The name of the cache file
+     * @param    int      $lifetime   The lifetime of the cache file in seconds (0 = infinite)
+     * @return   void
+     * @access   public
+     */
+    public function __construct($name, $lifetime = null) {
+        if (!is_dir(WW_CACHE_PATH))
+            mkdir(WW_CACHE_PATH);
+
+        if (!isset($lifetime))
+            $lifetime = ww_config('cache_lifetime', 86400);
+
+        $this->_name = $name;
+        $this->_lifetime = (int) $lifetime;
+    }
+
+    /**
+     * Reads data from cache. The $callback is used to generate the data if they are missing or expired.
+     * @param    callable   $callback   The callback function that returns the data to store
+     * @return   mixed
+     * @access   public
+     */
+    public function data($callback) {
+        if (!is_callable($callback)) {
+            trigger_error('Invalid callback given for cache instance "'.$this->_name.'"', E_USER_WARNING);
+            return;
+        }
+
+        if (ww_config('enable_caching') == true) {
+            // Caching is enabled, so we use a file
+			$filename = WW_CACHE_PATH.'/'.$this->_name.'.cache';
+
+            // Check if the file exists
+            if (file_exists($filename)) {
+                $file_content = file_get_contents($filename);
+                list($modified, $raw_data) = explode("\n", $file_content, 2);
+
+                // Check if the file has expired. If so, there is no data we could use.
+                if ($this->_lifetime > 0 && $modified + $this->_lifetime < time())
+                    $raw_data = null;
+            }
+
+            if (isset($raw_data)) {
+                // We were able to retrieve data from the file
+                return unserialize($raw_data);
+            } else {
+                // No data from file, so we use the data callback and store the given value
+                $data = $callback();
+
+                $file_content = time()."\n".serialize($data);
+                file_put_contents($filename, $file_content);
+
+                return $data;
+            }
+        } else {
+            // Caching is disabled, so we use the data callback
+            return $callback();
+        }
+    }
+
+}
