058152f4613c4b951b47c4691e0d5f5f42cf41b2
Christian Neff <christian.neff@gmail.com>
2014-03-08 19:39:53 +0100

    Upgraded I18n system to use Locales

--- install.sql
+++ install.sql
@@ -37,16 +37,22 @@ CREATE TABLE IF NOT EXISTS `ww_users` (
 INSERT INTO `ww_users` (`id`, `username`, `email`, `password`, `group`, `profile`, `lastactive`) VALUES
 (1, 'admin', 'example@example.com', '$2y$10$H6H2qdcXn6yYtshRHTiXMOEMh6ePW/VZ4J1b6nIjkGGRXVinQCOLy', 3, '{}', '0000-00-00 00:00:00');
 
-CREATE TABLE IF NOT EXISTS `ww_languages` (
+CREATE TABLE IF NOT EXISTS `ww_locales` (
   `id` varchar(5) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `direction` varchar(3) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `locales` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `text_direction` varchar(3) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `number_sep_decimal` varchar(2) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `number_sep_thousand` varchar(2) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fmt_money` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fmt_time` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fmt_date_short` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fmt_date_medium` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fmt_date_long` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `ww_languages` (`id`, `name`, `direction`, `locales`) VALUES
-('en-US', 'English', 'ltr', 'en_US.UTF-8,en_US,eng,English');
+INSERT INTO `ww_locales` (`id`, `name`, `text_direction`, `number_sep_decimal`, `number_sep_thousand`, `fmt_money`, `fmt_time`, `fmt_date_short`, `fmt_date_medium`, `fmt_date_long`) VALUES
+('en-US', 'English (US)', 'ltr', '.', ',', '$ #,###.##', 'H:i A', 'm/d/Y', 'F j, Y', 'D, F j, Y');
 
 CREATE TABLE IF NOT EXISTS `ww_translations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
--- libraries/International.php
+++ libraries/International.php
@@ -29,21 +29,21 @@
 class International {
     
     /**
-     * The currently used language pack
+     * The currently used locale pack
      * @var      string
      * @access   private
      * @static
      */
-    private static $_language;
+    private static $_locale;
     
     /**
-     * The data of the current language
+     * The data of the current locale
      * @var      array
      * @access   private
      * @static
      */
-    private static $_language_data;
-    
+    private static $_locale_data;
+
     /**
      * The translation engine object
      * @var      Translations
@@ -59,78 +59,138 @@ class International {
      * @static
      */
     public static function init() {
-        $languages = self::getAvailableLanguages();
+        $locales = self::getAvailableLocales();
         $default_lang = (string) ww_setting('main:lang');
 
-        if (!in_array($default_lang, $languages))
+        if (!in_array($default_lang, $locales))
             trigger_error('The default language is invalid or undefined', E_USER_ERROR);
         
         // Detect the user's preferred language
         if ($session_lang = SessionManager::read('language')) {
             // There was found a language setting in the user's session
             $detected_lang = $session_lang;
-        } elseif ($browser_lang = self::findBestBrowserLanguage($languages)) {
+        } elseif ($browser_lang = self::findBestBrowserLanguage($locales)) {
             // Try to find out the language using browser information
             $detected_lang = $browser_lang;
         }
 
         // If the preferred language is not supported, fall back to the default language
-        if (in_array($detected_lang, $languages)) {
-            $language = $detected_lang;
+        if (in_array($detected_lang, $locales)) {
+            $locale = $detected_lang;
         } else {
-            $language = $default_lang;
+            $locale = $default_lang;
         }
         
-        $cache = new Cache('languages/'.$language);
-        $language_data = $cache->data(function () use ($language) {
-            $sql = 'SELECT * FROM @PREFIX@languages WHERE id = {0}';
-            return System::db()->query($sql, [$language])->fetchAssoc();
+        $cache = new Cache('locales/'.$locale);
+        $locale_data = $cache->data(function () use ($locale) {
+            $sql = 'SELECT * FROM @PREFIX@locales WHERE id = {0}';
+            return System::db()->query($sql, [$locale])->fetchAssoc();
         });
         
-        self::$_language = $language;
-        self::$_language_data = $language_data;
-        
-        setlocale(LC_ALL, explode(',', $language_data['locales']));
+        self::$_locale = $locale;
+        self::$_locale_data = $locale_data;
 
-        self::$_t = new Translations($language);
+        self::$_t = new Translations($locale);
     }
     
     /**
-     * Returns the name of the currently used language pack
+     * Returns the name of the currently used locale pack
      * @return   string
      * @access   public
      * @static
      */
-    public static function getCurrentLanguage() {
-        if (!isset(self::$_language))
+    public static function getCurrentLocale() {
+        if (!isset(self::$_locale))
             trigger_error('The I18n system is not yet initialized', E_USER_ERROR);
         
-        return self::$_language;
+        return self::$_locale;
     }
 
     /**
-     * Returns the text direction of the language
+     * Returns the text direction of the locale
      * @return   string
      * @access   public
      * @static
      */
     public static function getTextDirection() {
-        if (!isset(self::$_language_data))
+        if (!isset(self::$_locale_data))
             trigger_error('The I18n system is not yet initialized', E_USER_ERROR);
         
-        return self::$_language_data['direction'];
+        return self::$_locale_data['text_direction'];
     }
-    
+
     /**
-     * Returns a list of available language packs
+     * Returns the number separators
      * @return   array
      * @access   public
      * @static
      */
-    public static function getAvailableLanguages() {
-        $cache = new Cache('languages/list');
+    public static function getNumberSeparators() {
+        if (!isset(self::$_locale_data))
+            trigger_error('The I18n system is not yet initialized', E_USER_ERROR);
+        
+        return [
+            'decimal'  => self::$_locale_data['number_sep_decimal'],
+            'thousand' => self::$_locale_data['number_sep_thousand']
+        ];
+    }
+
+    /**
+     * Returns the money format
+     * @return   string
+     * @access   public
+     * @static
+     */
+    public static function getMoneyFormat() {
+        if (!isset(self::$_locale_data))
+            trigger_error('The I18n system is not yet initialized', E_USER_ERROR);
+        
+        return self::$_locale_data['fmt_money'];
+    }
+
+    /**
+     * Returns the time format
+     * @return   string
+     * @access   public
+     * @static
+     */
+    public static function getTimeFormat() {
+        if (!isset(self::$_locale_data))
+            trigger_error('The I18n system is not yet initialized', E_USER_ERROR);
+        
+        return self::$_locale_data['fmt_time'];
+    }
+
+    /**
+     * Returns the date format
+     * @param    int      $length   The date length (1 = short [default], 2 = medium, 3 = long)
+     * @return   string
+     * @access   public
+     * @static
+     */
+    public static function getDateFormat($length = 1) {
+        if (!isset(self::$_locale_data))
+            trigger_error('The I18n system is not yet initialized', E_USER_ERROR);
+            
+        if ($length >= 3) {
+            return self::$_locale_data['fmt_date_long'];
+        } elseif ($length == 2) {
+            return self::$_locale_data['fmt_date_medium'];
+        } else {
+            return self::$_locale_data['fmt_date_short'];
+        }
+    }
+    
+    /**
+     * Returns a list of available locale packs
+     * @return   array
+     * @access   public
+     * @static
+     */
+    public static function getAvailableLocales() {
+        $cache = new Cache('locales/list');
         return $cache->data(function () {
-            return System::db()->select('@PREFIX@languages', 'id')->fetchColumn();
+            return System::db()->select('@PREFIX@locales', 'id')->fetchColumn();
         });
     }
     
